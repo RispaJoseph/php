85dc6a9c9103cfab3b73b73e2a77150c22c938f8
RispaJoseph <rizpahjoseph@gmail.com>
2023-05-12 01:16:02 +0530

    Added array pgm

---
+++ array.php
@@ -0,0 +1,7 @@
+<?php
+$names=array('Abel','Bob','Clair','Darcy');
+$names[4]="Erin";
+$names[5]="Folly";
+// echo $names[2];
+print_r($names);
+?>
---
+++ operators.php
@@ -0,0 +1,25 @@
+<?php
+$num1=10;
+$num2='10';
+
+if ($num1==$num2)
+{
+    echo "Equal<br>";
+}
+
+else
+{
+    echo "Not Equal";
+}
+
+if ($num1===$num2)
+{
+    echo "Equal type";
+}
+
+else
+{
+    echo "Not Equal type";
+}
+
+?>
