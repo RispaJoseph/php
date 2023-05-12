22352089dd0ac4d71ebc7d8ecf28bc96b0fa0428
RispaJoseph <rizpahjoseph@gmail.com>
2023-05-13 00:08:25 +0530

    Added Switch pgm

---
+++ switch.php
@@ -0,0 +1,22 @@
+<?php
+$num=10;
+switch($num)
+{
+    case 1:
+        echo "The number is One";
+        break;
+
+    case 2:
+        echo "The number is Two";
+        break;
+
+    case 3:
+        echo "The number is Three";
+        break;
+
+    default:
+        echo "Number not found!";
+        break;
+    
+}
+?>
