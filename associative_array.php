51a4553e3ac806bdb8de21da1ed3dce4dfa00907
RispaJoseph <rizpahjoseph@gmail.com>
2023-05-12 11:04:34 +0530

    Added multidimensional array pgm

--- associative_array.php
+++ associative_array.php
@@ -4,3 +4,6 @@ echo "The number of Apple is ".$name['Apple'].'<br><br>';
 
 print_r($name);
 ?>
+
+<!-- An associative array is in the form of key-value pair, 
+where the key is the index of the array and the value is the element of the array. -->
---
+++ multi-dimentional-array.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Students  Age  weight
+Mark      15    49
+Rose      17    56
+Zyan      14    42
+*/
+
+/* Multidimentional Array
+
+A multidimensional array is an array containing one or more arrays.
+Multidimensional arrays are used to store the data in a tabular form.
+
+*/
+
+$students=array(array('Mark',15,49),array('Rose',17,56),array('Zyan',14,42));
+
+print_r($students);
+echo "<br><br>";
+
+
+echo $students[0][0].'  '.$students[0][1].'  '.$students[0][2].'<br>';
+echo $students[1][0].'  '.$students[1][1].'  '.$students[1][2].'<br>';
+echo $students[2][0].'  '.$students[2][1].'  '.$students[2][2];
+?>
