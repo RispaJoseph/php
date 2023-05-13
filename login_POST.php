27c9efffa577987a1e92bf34dc745aa74aefad95
RispaJoseph <rizpahjoseph@gmail.com>
2023-05-13 23:58:16 +0530

    file added

---
+++ login_POST.php
@@ -0,0 +1,11 @@
+<html>
+    <body>
+        <head>Student Login</head><br><br>
+        <form action="POST.php" method="post">
+            <b>Username:</b><input type="text" name="stu_name">
+            <b>Password:</b><input type="text" name="stu_age">
+            <br><br>
+            <input type="Submit">
+        </form>
+    </body>
+</html>
