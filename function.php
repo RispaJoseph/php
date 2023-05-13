d20894aee7dfaec9ae5c65451629682a1c76e04e
RispaJoseph <rizpahjoseph@gmail.com>
2023-05-14 00:39:45 +0530

    file added

---
+++ function.php
@@ -0,0 +1,10 @@
+<?php
+function hello()
+{
+    echo "Hello Rispa<br>";
+    echo "How are you!<br>";
+    echo "<b>I'm you!</b><br>";
+}
+hello();
+hello();
+?>
---
+++ function_with_arguments.php
@@ -0,0 +1,10 @@
+<?php
+function add($num1,$num2)
+{
+    echo $num1+$num2.'<br>';
+}
+
+add(1,2);
+add(5,5);
+add(1997,26);
+?>
